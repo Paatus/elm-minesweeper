d084174d8a9a4a38a087c1264c442ca8861a6938
Pontus Hjortskog <pontus.hjortskog@kvd.se>
2020-02-17 15:54:37 +0100

    Updates

---
+++ precache-manifest.a0f230cbca4b57a394c3d7f17a9b7e12.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c840a951817d63b39551f4127aefa58",
+    "url": "/elm-minesweeper/bomb.svg"
+  },
+  {
+    "revision": "cf44fad4ab6f5ade82e73deac057e6fc",
+    "url": "/elm-minesweeper/dead.svg"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/elm-minesweeper/favicon.ico"
+  },
+  {
+    "revision": "bfbc21b49e5e60c32e035584ee178b4b",
+    "url": "/elm-minesweeper/flag.svg"
+  },
+  {
+    "revision": "bb1f4b2b8500d77e8ab8e56cea949099",
+    "url": "/elm-minesweeper/happy.svg"
+  },
+  {
+    "revision": "4c84104d5f89a22b4a58599548e2b7c2",
+    "url": "/elm-minesweeper/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/elm-minesweeper/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/elm-minesweeper/manifest.json"
+  },
+  {
+    "revision": "b445e7409e26a447bf22",
+    "url": "/elm-minesweeper/static/css/main.b45e1039.chunk.css"
+  },
+  {
+    "revision": "b445e7409e26a447bf22",
+    "url": "/elm-minesweeper/static/js/main.b445e740.chunk.js"
+  },
+  {
+    "revision": "4b0dacf4fad5255f38c7",
+    "url": "/elm-minesweeper/static/js/runtime~main.4b0dacf4.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/elm-minesweeper/static/js/vendors~main.3ca81432.chunk.js"
+  },
+  {
+    "revision": "6c840a951817d63b39551f4127aefa58",
+    "url": "/elm-minesweeper/static/media/bomb.6c840a95.svg"
+  },
+  {
+    "revision": "cf44fad4ab6f5ade82e73deac057e6fc",
+    "url": "/elm-minesweeper/static/media/dead.cf44fad4.svg"
+  },
+  {
+    "revision": "bfbc21b49e5e60c32e035584ee178b4b",
+    "url": "/elm-minesweeper/static/media/flag.bfbc21b4.svg"
+  },
+  {
+    "revision": "bb1f4b2b8500d77e8ab8e56cea949099",
+    "url": "/elm-minesweeper/static/media/happy.bb1f4b2b.svg"
+  },
+  {
+    "revision": "0e2dc0a9cc9e29d655b7f7c3666d6528",
+    "url": "/elm-minesweeper/static/media/very-happy.0e2dc0a9.svg"
+  },
+  {
+    "revision": "0e2dc0a9cc9e29d655b7f7c3666d6528",
+    "url": "/elm-minesweeper/very-happy.svg"
+  }
+]);
